0e60fadcbc0650dc8b0339b2e12b9a5894940f06
ElecFreaks <support@elecfreaks.com>
2018-01-30 17:51:47 +0800

    Update motorbit.ts

--- motorbit.ts
+++ motorbit.ts
@@ -15,39 +15,23 @@
 //% color="#333333" weight=10 icon="\uf1b9"
 namespace motorbit {
 
-    let M1_DIR = 0
-    let M2_DIR = 0
-    let M1_PWM = 1023
-    let M2_PWM = 1023
-
-    function stateupdate(): void {
-        
-        pins.digitalWritePin(DigitalPin.P8, M1_DIR)
-        pins.digitalWritePin(DigitalPin.P12, M1_DIR)
-        pins.analogWritePin(AnalogPin.P1, M1_PWM)
-        pins.analogWritePin(AnalogPin.P2, M2_PWM)
-    }
-
     /**
-     * TODO: describe your function here
-     * @param n the n from 0 (min) to 100 (max), eg:0
-     */
+ * TODO: describe your function here
+ * @param n the n from 0 (min) to 100 (max), eg:0
+ */
     //% blockId=motorbit_forward block="Move forward with speed %n"
     //% n.min=0 n.max=100
     export function forward(n: number): void {
         // Add code here
-        let speed = 0
+       
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
 
-        M1_DIR = 0
-        M2_DIR = 0
-
-        speed = n * 1023 / 100
-        M1_PWM = speed
-        M2_PWM = speed
-
-        stateupdate()
+        pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+        pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
 
     }
+
 
     /**
      * TODO: describe your function here
@@ -57,16 +41,14 @@ namespace motorbit {
     //% n.min=0 n.max=100
     export function back(n: number): void {
         // Add code here
-        let speed = 0
 
-        M1_DIR = 1
-        M2_DIR = 1
 
-        speed = n * 1023 / 100
-        M1_PWM = speed
-        M2_PWM = speed
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P12, 1)
 
-        stateupdate()
+        pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+        pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
+
 
     }
 
@@ -79,18 +61,15 @@ namespace motorbit {
     //% n.min=0 n.max=100
     export function turnleft(n: number): void {
         // Add code here
-        let speed = 0
 
-        M1_DIR = 0
-        M2_DIR = 0
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
 
-        speed = n * 1023 / 100
-        M1_PWM = 0
-        M2_PWM = speed
-
-        stateupdate()
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
 
     }
+
 
     /**
      * TODO: describe your function here
@@ -100,18 +79,15 @@ namespace motorbit {
     //% n.min=0 n.max=100
     export function turnright(n: number): void {
         // Add code here
-        let speed = 0
 
-        M1_DIR = 0
-        M2_DIR = 0
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
 
-        speed = n * 1023 / 100
-        M1_PWM = speed
-        M2_PWM = 0
-
-        stateupdate()
+        pins.analogWritePin(AnalogPin.P1, n * 1023 / 100)
+        pins.analogWritePin(AnalogPin.P2, 0)
 
     }
+
 
     /**
      * TODO: describe your function here
@@ -121,16 +97,15 @@ namespace motorbit {
     export function braking(): void {
         // Add code here
 
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
 
-        M1_DIR = 0
-        M2_DIR = 0
-
-        M1_PWM = 0
-        M2_PWM = 0
-
-        stateupdate()
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, 0)
 
     }
+
+
 
     /**
     * TODO: describe your function here
@@ -143,29 +118,24 @@ namespace motorbit {
     export function freestyle(m: number, n: number): void {
         // Add code here
 
-
         if (m > 0) {
-            M1_DIR = 0
-            M1_PWM = m * 1023 / 100
-        } 
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.analogWritePin(AnalogPin.P1, m * 1023 / 100)
+        }
         else {
-            M1_DIR = 1
-            M1_PWM = -m * 1023 / 100           
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.analogWritePin(AnalogPin.P1, - m * 1023 / 100)
         }
 
         if (n > 0) {
-            M2_DIR = 0
-            M2_PWM = n * 1023 / 100
-        } else 
-        {
-            M2_DIR = 1
-            M2_PWM = -n * 1023 / 100
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
+        } else {
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.analogWritePin(AnalogPin.P2, - n * 1023 / 100)
         }
 
-        pins.digitalWritePin(DigitalPin.P8, M1_DIR)
-        pins.digitalWritePin(DigitalPin.P12, M1_DIR)
-        pins.analogWritePin(AnalogPin.P1, M1_PWM)
-        pins.analogWritePin(AnalogPin.P2, M2_PWM)
+
 
     }
 
